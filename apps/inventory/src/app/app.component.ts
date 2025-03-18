2dad36448ac319d461112004a3d866ba0d1c3028
Juanma2325 <juangarcia.external@institutodeinnovacion.es>
2025-03-18 10:50:42 +0100

    Tutorial de angular nx.dev casi terminado.

--- apps/inventory/src/app/app.component.ts
+++ apps/inventory/src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 import { ProductsComponent } from '@angular-monorepo/products';
+import { NxWelcomeComponent } from './nx-welcome.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
-  imports: [ProductsComponent],
+  imports: [ProductsComponent, NxWelcomeComponent, RouterOutlet],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
